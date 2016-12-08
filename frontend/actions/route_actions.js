3c0edccd349eb915dcc855389cf4fef5b542f3d4
jmolinari <josephtmolinari@gmail.com>
2016-12-08 14:24:28 -0500

    Added route actions

---
+++ frontend/actions/route_actions.js
@@ -0,0 +1,25 @@
+import * as APIUtil from '../util/route_api_util';
+export const RECEIVE_ROUTE = 'RECEIVE_ROUTE';
+export const RECEIVE_ROUTE_ERRORS = 'RECEIVE_ERRORS';
+export const CLEAR_ERRORS = 'CLEAR_ERRORS';
+
+export const receiveRoute = (route) => ({
+  type: RECEIVE_ROUTE,
+  route: route
+});
+
+export const receiveRouteErrors = (errors) => ({
+  type: RECEIVE_ROUTE_ERRORS,
+  errors: errors
+});
+
+export const clearErrors = () => ({
+  type: CLEAR_ERRORS
+});
+
+export function createRoute(route) {
+  return (dispatch) => {
+    return APIUtil.createRoute(route).then((route) => dispatch(receiveRoute(route)),
+      (err) => dispatch(receiveRouteErrors(err.responseJSON)));
+  };
+};
