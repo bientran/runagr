7855be16f337ef0797ed688c45f96ba59866799c
jmolinari <josephtmolinari@gmail.com>
2016-12-13 12:34:44 -0500

    Refactored out the activity fetching

--- frontend/components/dashboard/dashboard.jsx
+++ frontend/components/dashboard/dashboard.jsx
@@ -3,37 +3,61 @@ import { Link, withRouter } from 'react-router';
 import SessionFormContainer from '../session/session_form_container';
 import MainNav from '../nav/main_nav';
 import ActivityFeedContainer from '../activity/activity_feed/activity_feed_container';
+import ActivityStats from '../activity/activity_stats/activity_stats';
+import { values } from 'lodash';
 
-const newSessionLinks = (logout) => (
-  <nav>
-    <Link to="/login">Login</Link>
-    <br />
-    <Link to="/signup">Sign Up</Link>
-  </nav>
-);
+// const newSessionLinks = (logout) => (
+//   <nav>
+//     <Link to="/login">Login</Link>
+//     <br />
+//     <Link to="/signup">Sign Up</Link>
+//   </nav>
+// );
+//
+// const handleLogout = (logout, router, clearUser) => {
+//   return () => {
+//     clearUser();
+//     logout();
+//     router.replace("/login");
+//   }
+// };
+//
+//
+// const logoutLink = (currentUser, logout, router, clearUser) => (
+//   <div>
+//     <h4>Current User: {currentUser.first_name} {currentUser.last_name}</h4>
+//     <button onClick={handleLogout(logout, router, clearUser)}>Log Out</button>
+//   </div>
+// );
 
-const handleLogout = (logout, router, clearUser) => {
-  return () => {
-    clearUser();
-    logout();
-    router.replace("/login");
+class Dashboard extends React.Component {
+
+  componentDidMount() {
+    this.props.fetchAllActivities(this.props.currentUser.id);
   }
-};
 
+  render() {
+    let activities = this.props.activities
+    if('activity' in activities) {
+      return (<div></div>);
+    }
+    return(
+      <section className="dashboard">
+        <ActivityFeedContainer />
+        <ActivityStats activities={values(activities)} />
+      </section>
+    );
+  }
 
-const logoutLink = (currentUser, logout, router, clearUser) => (
-  <div>
-    <h4>Current User: {currentUser.first_name} {currentUser.last_name}</h4>
-    <button onClick={handleLogout(logout, router, clearUser)}>Log Out</button>
-  </div>
-);
+}
 
-const Dashboard = ({currentUser, logout, router, clearUser}) => {
-  // {currentUser ? logoutLink(currentUser, logout, router, clearUser) : newSessionLinks(logout)}
-
-  // <div></div>
-  return(
-    <ActivityFeedContainer />
-)};
+//
+// const Dashboard = ({currentUser, logout, router, clearUser}) => {
+//   // {currentUser ? logoutLink(currentUser, logout, router, clearUser) : newSessionLinks(logout)}
+//
+//   // <div></div>
+//   return(
+//     <ActivityFeedContainer />
+// )};
 
 export default withRouter(Dashboard);
--- frontend/components/dashboard/dashboard_container.js
+++ frontend/components/dashboard/dashboard_container.js
@@ -1,14 +1,17 @@
 import { connect } from 'react-redux';
 import { logout, clearUser } from '../../actions/session_actions';
+import { fetchAllActivities } from '../../actions/activity_actions';
 import Dashboard from './dashboard';
 
 const mapStateToProps = (state) => ({
-  currentUser: state.session.currentUser
+  currentUser: state.session.currentUser,
+  activities: state.activity,
 });
 
 const mapDispatchToProps = (dispatch) => ({
   logout: () => dispatch(logout()),
-  clearUser: () => dispatch(clearUser())
+  clearUser: () => dispatch(clearUser()),
+  fetchAllActivities: () => dispatch(fetchAllActivities())
 });
 
 export default connect(
