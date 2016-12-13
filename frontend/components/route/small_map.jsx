f053711ce84e69db8d009c68aed93dffe39c6fc5
jmolinari <josephtmolinari@gmail.com>
2016-12-12 22:38:46 -0500

    Allowed optional sizeprops

--- frontend/components/route/small_map.jsx
+++ frontend/components/route/small_map.jsx
@@ -4,20 +4,36 @@ class SmallMap extends React.Component {
 
   encodePath() {
     let path = [];
-    JSON.parse(this.props.path).forEach((coord) => path.push(`${coord.lat},${coord.lng}`));
+    // console.log(this.props.path);
+    if (typeof this.props.path !== "object"){
+      JSON.parse(this.props.path).forEach((coord) => path.push(`${coord.lat},${coord.lng}`));
+    } else {
+      this.props.path.forEach((coord) => path.push(`${coord.lat},${coord.lng}`));
+    }
     return path.join("|");
   }
 
   staticMapUrl() {
     let urlBase = "https://maps.googleapis.com/maps/api/staticmap?";
-    let size = `size=200x200`;
+    // let size = `size=200x200`;
+    let size;
+    if(this.props.x){
+      size = `size=${this.props.x}x${this.props.y}`;
+    } else {
+      size = `size=200x200`;
+    }
     let path = `&path=weight:3%7Ccolor:blue|${this.encodePath()}`;
     let key = "&key=AIzaSyBIpNsO03UZ-PFgUmeL-RZAeZDIlgVmC8c";
     return urlBase + size + key +path ;
   }
 
   fixImage() {
-    return "https://maps.googleapis.com/maps/api/staticmap?size=200x200&key=AIzaSyBIpNsO03UZ-PFgUmeL-RZAeZDIlgVmC8c";
+    let size;
+    if(this.props.x){
+      return `https://maps.googleapis.com/maps/api/staticmap?size=${this.props.x}x${this.props.y}&key=AIzaSyBIpNsO03UZ-PFgUmeL-RZAeZDIlgVmC8c`;
+    } else {
+      return "https://maps.googleapis.com/maps/api/staticmap?size=200x200&key=AIzaSyBIpNsO03UZ-PFgUmeL-RZAeZDIlgVmC8c";
+    }
   }
 
   render () {
